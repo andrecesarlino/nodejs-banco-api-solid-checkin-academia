e052433d3af9774679cfe78b3a1aa4f302733dea
Andre Lino <andre.cesar97@hotmail.com>
2023-09-06 16:24:07 -0300

    ajustado os repositorios e erros

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,6 @@
-import { registerService } from '@/services/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import {z} from 'zod'
 
@@ -12,13 +14,20 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const {name,email,password} = registerBodySchema.parse(request.body)
 
   try {
-    await registerService({
+    const prismaUserRepository = new PrismaUsersRepository()
+    const registerUserCase = new RegisterUseCase(prismaUserRepository)
+
+    await registerUserCase.execute({
       name,
       email,
       password,
     })
   } catch (err) {
-    return reply.status(409).send()
+    if(err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({message: err.message})
+    }
+    throw err
+    //return reply.status(500).send()
   }
 
   return reply.status(201).send()
